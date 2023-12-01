63291827f1bbe27a26216499b09d48372129206e
IceBlazer <72805987+IceBlazer@users.noreply.github.com>
2023-11-30 22:50:24 -0500

    started Stack Navigation for the first two screens

---
+++ routes/homeStack.js
@@ -0,0 +1,17 @@
+/* import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import {createAppContainer} from 'react-navigation';
+import Home from '../screens/home';
+import PortfolioBuilder from '../screens/portfolioBuilder';
+
+const screens = {
+    Home: {
+        screen: Home
+    },
+    PortfolioBuilder: {
+        screen: PortfolioBuilder
+    }
+    
+}
+const HomeStack = createNativeStackNavigator();
+
+//export default createAppContainer(HomeStack); */

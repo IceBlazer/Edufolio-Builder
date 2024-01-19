92dca6e2ec190415d5268cea7b607d7e46c5d98f
IceBlazer <72805987+IceBlazer@users.noreply.github.com>
2024-01-19 12:40:47 -0500

    Added comments

--- AppContext.js
+++ AppContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 const AppContext = createContext();
 
-const initialState = {
+const initialState = { //initialState is the initial state of the application. It is an object with the following properties(categories) and their initial values:
   
   personalInfo: [],
   sports: [],
@@ -17,10 +17,10 @@ const initialState = {
   hcs: [],
   aI: [],
 
-  // ... other state variables
 };
 
-const reducer = (state, action) => {
+const reducer = (state, action) => { 
+  //reducer is a function that takes the current state and an action as arguments and returns a new state based on that action. It is a pure function that only depends on the arguments passed to it. It does not depend on any external state or change its arguments.
   switch (action.type) {
     
     case 'SET_PERSONALINFO':
@@ -50,7 +50,7 @@ const reducer = (state, action) => {
   }
 };
 
-const AppProvider = ({ children }) => {
+const AppProvider = ({ children }) => { //AppProvider is a component that provides the application state to all its children. It takes the children as a prop and returns the AppContext.Provider component with the value prop set to the current state and the dispatch function.
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
@@ -60,7 +60,7 @@ const AppProvider = ({ children }) => {
   );
 };
 
-const useAppContext = () => {
+const useAppContext = () => { //useAppContext is a custom hook that returns the current state and the dispatch function. It is a wrapper around the useContext hook.
   const context = useContext(AppContext);
   if (!context) {
     throw new Error('useAppContext must be used within an AppProvider');
